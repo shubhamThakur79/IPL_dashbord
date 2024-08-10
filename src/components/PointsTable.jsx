86a362b2cb186155606a88c07f7f300b138cf69f
Shubham Thakur <shubham.20102005.bca22@engg.hgpi.in>
2024-08-11 01:23:57 +0530

    new

--- src/components/PointsTable.jsx
+++ src/components/PointsTable.jsx
@@ -1,11 +1,10 @@
-
 import React, { useEffect, useState } from 'react';
-import { FaCheck } from "react-icons/fa";
-import { RxCross2 } from "react-icons/rx";
+import { FaCheck } from 'react-icons/fa';
+import { RxCross2 } from 'react-icons/rx';
 
 const PointsTable = () => {
     const [tableData, setTableData] = useState(null);
- 
+
     async function fetchPointsTable() {
         const url = 'https://cricbuzz-cricket.p.rapidapi.com/stats/v1/series/7607/points-table';
         const options = {
@@ -21,65 +20,63 @@ const PointsTable = () => {
             const result = await response.json();
             setTableData(result?.pointsTable[0]?.pointsTableInfo || []);
         } catch (error) {
-            console.error(error);
+            console.error('Error fetching points table data:', error);
         }
     }
 
     useEffect(() => {
         fetchPointsTable();
-        // setTableData(pointsTableData.pointsTable[0].pointsTableInfo)
-
     }, []);
+
+    if (tableData === null) {
+        return <div className='w-full text-center mt-3'>Loading...</div>;
+    }
 
     return (
         <div className='w-full lg:ml-1 mt-3'>
-
-            <table className='w-full text-center mx-auto  '>
-                <tr className='w-full h-8 text-center mx-auto text-gray-300/70 border-b overflow-hidden  '>
-                    <td className='w-[20%]  lg:w-auto '>Team</td>
-                    <td>M</td>
-                    <td>W</td>
-                    <td>L</td>
-                    <td>NRR</td>
-                    <td>Pts</td>
-                    <td>Last 5</td>
-                </tr>
-
-
-
-                {tableData === null ? (
-                    <h1>Loading...</h1>
-                ) : (
-                    tableData.map((table, i) => (
-                        <tr className='h-[40px] border-b border-gray-400/40 hover:bg-gray-300/20 ' key={i}>
-                            <td className='flex lg:gap-1 mr-1   lg:w-[80%] items-center justify-center'>{i + 1}
-                                <img className='w-5 h-5 mr-2 object-contain' src={`https://res.cloudinary.com/digkgdovw/image/upload/v1715267905/iplTeamLogo/${table?.teamImageId}`} alt={`${table?.teamFullName} logo`} />
-                                {table?.teamName}
-                                           </td>
+            <table className='w-full text-center mx-auto border-collapse'>
+                <thead>
+                    <tr className='h-8 text-center text-gray-300/70 border-b'>
+                        <td className='w-[20%] lg:w-auto'>Team</td>
+                        <td>M</td>
+                        <td>W</td>
+                        <td>L</td>
+                        <td>NRR</td>
+                        <td>Pts</td>
+                        <td>Last 5</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {tableData.map((table, i) => (
+                        <tr className='h-[40px] border-b border-gray-400/40 hover:bg-gray-300/20' key={i}>
+                            <td className='flex items-center justify-center'>
+                                <img className='w-5 h-5 mr-2 object-contain' 
+                                    src={`https://res.cloudinary.com/digkgdovw/image/upload/v1715267905/iplTeamLogo/${table?.teamImageId}`} 
+                                    alt={`${table?.teamFullName} logo`} />
+                                {i + 1}. {table?.teamName}
+                            </td>
                             <td>{table?.matchesPlayed}</td>
                             <td>{table?.matchesWon}</td>
                             <td>{table?.matchesLost}</td>
                             <td>{table?.nrr}</td>
                             <td>{table?.points}</td>
-                            <td className='h-full text-center flex justify-center items-center ml-[-2px'>
+                            <td className='flex justify-center items-center'>
                                 {[...table.form].reverse().map((result, j) => (
-                                    <td key={j} className=''>
-
+                                    <span key={j} className='ml-1'>
                                         {result === "W" ? (
-                                            <FaCheck className='h-4 text-sm ml-[7px] p-[2px] text-white rounded-[50%] w-4 bg-green-500' />
+                                            <FaCheck className='h-4 text-sm p-[2px] text-white rounded-full w-4 bg-green-500' />
                                         ) : (
-                                            <RxCross2 className='bg-red-400 text-sm h-4 ml-2 p-[2px] text-white rounded-[50%] w-4' />
+                                            <RxCross2 className='h-4 text-sm p-[2px] text-white rounded-full w-4 bg-red-400' />
                                         )}
-                                    </td>
+                                    </span>
                                 ))}
                             </td>
                         </tr>
-                    ))
-                )}
+                    ))}
+                </tbody>
             </table>
         </div>
     );
 };
 
 export default PointsTable;
-
